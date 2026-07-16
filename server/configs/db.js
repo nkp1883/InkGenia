be1facbb57e267ee41e9ec6b8aebd2e8046b5597
pandey1883 <justunikk4459@gmail.com>
2026-07-16 23:11:39 +0530

    ...

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () =>{
     try {
         mongoose.connection.on('connected', ()=> console.log("Database Connected")
         )
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickblog`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/inkgenia`)
     } catch (error) {
         console.log(error.message);
     }
